99f8f1b1ce847fb9deb5e0238acc2e9d8b8c97dc
Hamim Ahmed <hamim.ahmed541@gmail.com>
2025-02-20 23:10:10 +0600

    js day1

---
+++ Day_15_JAVASCRIPT/Day_One/types.js
@@ -0,0 +1,14 @@
+// STRING
+var name = 'Hamim';
+var address = "Uttara Dhaka";
+console.log(address)
+
+// NUMERIC
+var weight = 20;
+var price = 10;
+console.log(price);
+
+// BOOLEAN
+var isRich = true;
+console.log(isRich);
+console.log(typeof isRich);
---
+++ Day_15_JAVASCRIPT/Day_One/variable.js
@@ -0,0 +1,4 @@
+var weight = 20;
+var price = 10;
+
+console.log(price);
