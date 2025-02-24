00c2b3dc91e767e572aa672e930bb04e7f3efb06
Hamim Ahmed <hamim.ahmed541@gmail.com>
2025-02-25 01:50:25 +0600

    day 2 js

---
+++ Day_15_JAVASCRIPT/Day_Two/comparion.js
@@ -0,0 +1,14 @@
+console.log(5>10);
+console.log(5<10);
+console.log(5!==10);
+console.log(10==10);
+console.log(15>=10); 
+console.log(100<=10);
+
+console.log(10==='10');
+console.log(10===10);
+
+var grapePrice = 40;
+var applePrice = 130;
+
+console.log(grapePrice>applePrice) 
