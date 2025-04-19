9afb3b88e4d7f10c270918cbaa98ceea705655f4
Hamim Ahmed <hamim.ahmed541@gmail.com>
2025-04-20 01:14:56 +0600

    ja day-9 done

---
+++ Day_15_JAVASCRIPT/Day_Nine/date&time.js
@@ -0,0 +1,6 @@
+const today = new Date();
+console.log(today);
+
+const now = new Date(); // Current date and time
+const specificDate = new Date('2025-04-20T12:30:00'); // Specific date and time
+const fromParts = new Date(2025, 3, 20, 12, 30); // Note: Month is 0-indexed (0 = Jan, 3 = April)
---
+++ Day_15_JAVASCRIPT/Day_Nine/math.js
@@ -0,0 +1,15 @@
+const min = Math.min(43, 45, 53, 34, 25, 3, 24, 34, 32, 34, 41, 4, 35, 4, 6, 54)
+console.log(min);
+const max = Math.max(43, 45, 53, 34, 25, 3, 24, 34, 32, 34, 41, 4, 35, 4, 6, 54)
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-88));
+
+console.log(Math.round(1.998))
+console.log(Math.floor(1.998))
+console.log(Math.ceil(1.998))
+
+console.log(Math.random() * 10);
+const rand = Math.round(Math.random() * 10)
+console.log(rand)
