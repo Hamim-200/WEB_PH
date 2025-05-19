c07f1f829e917ac6a4ebc0007618b780f32144a0
Hamim Ahmed <hamim.ahmed541@gmail.com>
2025-05-20 00:34:19 +0600

    day 17 project added

---
+++ Day_17_PAYOO_MOBILE_BANK/js/login.js
@@ -0,0 +1,18 @@
+document.getElementById('button-login').addEventListener('click', function (event) {
+
+    // prevent default behaviour (prevent reloading browser)
+    event.preventDefault();
+
+    //get the phpne & pin number
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, ' ', pinNumber);
+
+    //validation phone & pin
+    if (phoneNumber === '0123456789' && pinNumber === '1234') {
+        window.location.href = '/home.html'
+    }
+    else {
+        alert('Please Try Again');
+    }
+})
