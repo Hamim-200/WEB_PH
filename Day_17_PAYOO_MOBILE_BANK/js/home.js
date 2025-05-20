7dd3cadac492cf38b683d9cd028ecc51f98b3a39
Hamim Ahmed <hamim.ahmed541@gmail.com>
2025-05-20 09:56:48 +0600

    add money feature added

--- Day_17_PAYOO_MOBILE_BANK/js/home.js
+++ Day_17_PAYOO_MOBILE_BANK/js/home.js
@@ -1,12 +1,21 @@
 // Add money to the account
 document.getElementById('button-add-money').addEventListener('click', function (event) {
     event.preventDefault();
-
     // get money to be added to the account
     const addMoney = document.getElementById('input-add-money').value;
-    console.log(addMoney);
-
     //get the pin number
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput)
+    if (pinNumberInput === '1234') {
+        //get the current ballance
+        const balance = document.getElementById('account-balance').innerText;
+        // add new money in total balance
+        const addMoneyInput = parseFloat(addMoney)
+        const balanceNumber = parseFloat(balance)
+        const newBalance = addMoneyInput + balanceNumber;
+        //update the balance 
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+        alert('wrong')
+    }
 })
