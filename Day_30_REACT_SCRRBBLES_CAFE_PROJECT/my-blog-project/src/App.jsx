7566d90eccb1a51ffe755cc4ef3d12074a848207
Hamim Ahmed <hamim.ahmed541@gmail.com>
2026-01-11 23:15:40 +0600

    day-40 start

---
+++ Day_30_REACT_SCRRBBLES_CAFE_PROJECT/my-blog-project/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar></Navbar>
+    
+
+
+
+    <div className="main-container flex text-center">
+      <div className="left-container w-[70%]">
+        
+        <Blogs></Blogs>
+      </div>
+      <div className="right-container w-[30%] border-2">
+            <h1>Reading Time : 0</h1>
+            <h1>BookMarked count : 0</h1>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
